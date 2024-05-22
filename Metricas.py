659caf1886d4c925c14a9ba56e0b65b9bd8105d8
nazarenomm <nazarenomm98@gmail.com>
2024-05-22 15:42:16 -0300

    f1-score binario

--- Metricas.py
+++ Metricas.py
@@ -11,7 +11,6 @@ class Metricas:
         if not isinstance(y_true, list):
             y_true = y_true.tolist()
 
-
         f1_scores = []
         verdaderos_p_total = 0
         falsos_p_total = 0
@@ -21,7 +20,6 @@ class Metricas:
         clases = pd.Series(y_true).unique()
 
         for clase in clases:
-            # esto seria _recall y _precision pero un metodo estatico no tiene acceso
             combinada = list(zip(y_true, y_pred))
             
             verdaderos_p = sum(1 for y_t, y_p in combinada if y_p == clase and y_t == clase)
@@ -43,25 +41,27 @@ class Metricas:
                 f1 = 0
             f1_scores.append(f1)
 
-            if promedio == "macro":
-                retorno = sum(f1_scores) / len(f1_scores) if f1_scores else 0
+        if promedio == "macro":
+            f1_score = sum(f1_scores) / len(f1_scores) if f1_scores else 0
 
-            elif promedio == "micro":
-                precision_global = verdaderos_p_total / (verdaderos_p_total + falsos_p_total)
-                recall_global = verdaderos_p_total / (verdaderos_p_total + falsos_n_total)
+        elif promedio == "micro":
+            precision_global = verdaderos_p_total / (verdaderos_p_total + falsos_p_total)
+            recall_global = verdaderos_p_total / (verdaderos_p_total + falsos_n_total)
 
-                retorno = 2 * (precision_global * recall_global) / (precision_global + recall_global)
+            f1_score = 2 * (precision_global * recall_global) / (precision_global + recall_global)
 
-            elif promedio == "binario":
-                if len(clases) != 2:
-                    raise ValueError("Promedio binario no es válido en problemas multiclase")
-                
-            elif promedio == "ponderado": # seria weighted en sklearn
-                pass
-            elif promedio == "muestras":
-                pass
+        elif promedio == "binario":
+            if len(clases) != 2:
+                raise ValueError("Promedio binario no es válido en problemas multiclase")
+            
+            f1_score = f1_scores[0]
 
-        return retorno
+        elif promedio == "ponderado": # seria weighted en sklearn
+            pass
+        elif promedio == "muestras":
+            pass
+
+        return f1_score
 
     @staticmethod
     def accuracy_score(y_true: pd.Series | list, y_pred: list):
