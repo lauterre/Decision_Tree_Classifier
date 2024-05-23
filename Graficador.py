127a397ecf6211796e19bd6cad2d44c0ad4988f8
Lautaro Terreno <lauuterreno@gmail.com>
2024-05-23 00:57:38 -0300

    Graficador-va mejorando

--- Graficador.py
+++ Graficador.py
@@ -3,27 +3,22 @@ import matplotlib.pyplot as plt
 from collections import defaultdict  
 
 class TreePlot:
-    def __init__(self, arbol, ax=None, fontsize=0.1):
+    def __init__(self, arbol, ax=None, fontsize=None):
         self.arbol = arbol
         self.ax = ax
         self.fontsize = fontsize
 
     def plot(self):
         if self.ax is None:
-            _, self.ax = plt.subplots(figsize=(self._ancho_max(self.arbol), self.arbol.altura())) #self.get_fig_size()
+            _, self.ax = plt.subplots(figsize=(self._get_fig_size()))
         self.ax.clear()
         self.ax.set_axis_off()
-        xlim = (-1, 1) # deberia depender del ancho max
-        ylim = (-1, 1) # deberia depender de la altura
-        self.ax.set_xlim(xlim)
-        self.ax.set_ylim(ylim)
-        self._plot_tree(self.arbol, xy=(0, 1), level_width=len(xlim), level_height = len(ylim) *3/10, depth=0) # level_height = 0.6, width = 2
+        self._plot_tree(self.arbol, xy=(self._ancho_nivel(self.arbol), self.arbol.altura()), level_width=self._ancho_nivel(self.arbol))
         plt.show()
 
-    def _plot_tree(self, arbol, xy, level_width, level_height, depth):
-
+    def _plot_tree(self, arbol, xy, level_width, level_height=1, depth=0):
         bbox_args = dict(boxstyle="round", fc=self.get_color(arbol, arbol.clase) if arbol.es_hoja() else "white", ec="black")
-        caja = self._crear_caja(arbol, arbol.es_raiz())
+        caja = self._crear_caja(arbol)
         self._annotate(caja, xy, depth, bbox_args)
 
         if arbol.subs:
@@ -40,9 +35,9 @@ class TreePlot:
             for subarbol, new_xy in child_positions:
                 self._plot_tree(subarbol, new_xy, width_per_node, level_height, depth + 1)
 
-    def _crear_caja(self, arbol: Arbol, es_raiz: bool) -> str:
+    def _crear_caja(self, arbol):
         retorno = []
-        if arbol.categoria:
+        if not arbol.es_raiz():
             retorno.append(f"Categoría: {arbol.categoria}")
         retorno.append(f"Muestras: {arbol._total_samples()}")
         retorno.append(f"Valores: {arbol._values()}")
@@ -54,44 +49,55 @@ class TreePlot:
         return "\n".join(retorno)
 
     def _annotate(self, text, xy, depth, bbox_args):
+        dynamic_fontsize = self._calculate_fontsize()
         kwargs = dict(
             bbox=bbox_args,
             ha="center",
             va="center",
             zorder=100 - 10 * depth,
             xycoords="data",
-            fontsize = self.fontsize
+            fontsize=dynamic_fontsize
         )
         self.ax.annotate(text, xy=xy, **kwargs)
 
-    # def _get_fig_size(self):
-    #     prof = self.arbol.altura()
-    #     ancho_max = self._ancho_max(self.arbol)
-    #     ancho = max(10, ancho_max * 2)
-    #     altura = max(10, prof * 2)
-    #     return (ancho, altura)
+    def _calculate_fontsize(self):
+        num_nodes = self._count_nodes(self.arbol)
+        base_size = 10  # base fontsize
+        return max(2, base_size - num_nodes // 10)
 
-    def _ancho_max(self, arbol, nivel=0, nodos=None): # en Arbol
-        if nodos is None:
+    def _get_fig_size(self):
+        prof = self.arbol.altura()
+        ancho_max = self._ancho_max(self.arbol)
+        ancho = max(1, ancho_max * 2)
+        altura = max(1, prof)
+        return (ancho, altura)
+
+    def _ancho_max(self, arbol, nivel=0, nodos=None):
+        if nodos == None:
             nodos = defaultdict(int)
         nodos[nivel] += 1
         for subarbol in arbol.subs:
             self._ancho_max(subarbol, nivel + 1, nodos)
         return max(nodos.values())
-    
-    # Podria servir
-    def _ancho_nivel(self, arbol, nivel = 0, nodos= None): # deberia estar en Arbol
-        if nodos is None:
+
+    def _ancho_nivel(self, arbol, nivel=0, nodos=None):
+        if nodos == None:
             nodos = defaultdict(int)
         nodos[nivel] += 1
         for subarbol in arbol.subs:
             self._ancho_nivel(subarbol, nivel + 1, nodos)
         return nodos[nivel]
-    
+
+    def _count_nodes(self, arbol):
+        if not arbol.subs:
+            return 1
+        return 1 + sum(self._count_nodes(sub) for sub in arbol.subs)
+
     def get_color(self, arbol, clase):
         clases = arbol.target_categorias
-        colores = ["lightgreen", "lightblue", (1, 1, 0.6)] #agregar
-        colores_clases = {}
-        for i, c in enumerate(clases):
-            colores_clases[c] = colores[i]
-        return colores_clases[clase]
+        colores = ["lightgreen", "lightblue", (1, 1, 0.6)]
+        colores_clases = {c: colores[i] for i, c in enumerate(clases)}
+        return colores_clases.get(clase, "white")
+    
+    
+
